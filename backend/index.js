09afe1b60fbf5db36dac7dce604d291876f7dae8
YourName <your-email@example.com>
2026-02-11 14:33:49 +0100

    app vercejson

--- backend/index.js
+++ backend/index.js
@@ -30,4 +30,4 @@ app.use("/api/return", returnRoutes);
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+export default app;
